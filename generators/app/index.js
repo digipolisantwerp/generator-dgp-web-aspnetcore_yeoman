b8adc236ca9f33bdbd443f0ba3e89684b6d4cbce
Florian Ramakers <florian.ramakers@continuous.be>
2020-02-26 12:12:21 +0100

    updated dependencies + updated gulp to 4.0 + removed lock files + removed nexus repo + yeoman refactor for latest version + improvements

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,20 @@
 'use strict';
-const yeoman = require('yeoman-generator');
+const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 const del = require('del');
 const nd = require('node-dir');
-const Guid = require('guid');
+const {v4: uuidv4} = require('uuid');
 const updateNotifier = require('update-notifier');
 const pkg = require('./../../package.json');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = class extends Generator {
 
-  prompting: function () {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  prompting() {
     let done = this.async();
 
     // greet the user
@@ -18,7 +22,7 @@ module.exports = yeoman.generators.Base.extend({
 
     let notifier = updateNotifier({
       pkg,
-      updateCheckInterval: 1000 * 60 * 5      // check every 5 minutes.
+      updateCheckInterval: 1000 * 60 * 5 // check every 5 minutes.
     });
     notifier.notify();
 
@@ -28,24 +32,24 @@ module.exports = yeoman.generators.Base.extend({
     let prompts = [{
       type: 'confirm',
       name: 'deleteContent',
-      message: 'Delete the contents of this directory before generation (.git will be preserved) ?:',
+      message: 'Delete the contents of this directory before generation (.git will be preserved)?:',
       default: true
     },
       {
         type: 'input',
         name: 'projectName',
-        message: "Enter the name of the new project (don't forget the Pascal-casing):"
+        message: "Enter a name for your new project (preferably in PascalCase):"
       },
       {
         type: 'confirm',
         name: 'hasAppConfig',
-        message: 'Does the project have an Appconfig configuration ?',
+        message: 'Does the project have an AppConfig configuration?',
         default: false
       },
       {
         type: 'input',
         name: 'appConfigGuid',
-        message: 'Please enter the application unique key from Appconfig:',
+        message: 'Please enter the unique application key from AppConfig:',
         when: function (response) {
           return response.hasAppConfig;
         }
@@ -59,7 +63,7 @@ module.exports = yeoman.generators.Base.extend({
       {
         type: 'input',
         name: 'appUMName',
-        message: 'Please enter the the UME application name:',
+        message: 'Please enter the UME application name:',
         when: function (response) {
           return response.hasUMEName;
         }
@@ -67,53 +71,54 @@ module.exports = yeoman.generators.Base.extend({
 
     ];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;     // To access props later use this.props.someOption;
+    this.prompt(prompts).then((props) => {
+      this.props = props;
       done();
-    }.bind(this));
-  },
+    });
+  }
 
-  writing: function () {
+  writing() {
+    let done = this.async();
 
-    // empty target directory
-    console.log('Emptying target directory...');
-    if (this.props.deleteContent === true) {
+    /**
+     * Empty the target directory first if the user wished to do so.
+     * */
+    if (this.props.deleteContent) {
+      this.log('Emptying target directory... Starting fresh.');
       del.sync(['**/*', '!.git', '!.git/**/*'], {force: true, dot: true});
     }
 
     let projectName = this.props.projectName;
     let lowerProjectName = projectName.toLowerCase();
-    let probableUrlName = lowerProjectName.endsWith("web") ? lowerProjectName.substr(0, lowerProjectName.length - 3) : lowerProjectName;
+    let probableUrlName = lowerProjectName.endsWith('web') ? lowerProjectName.substr(0, lowerProjectName.length - 3) : lowerProjectName;
     let probableUmeName = probableUrlName.toUpperCase();
 
-    let solutionItemsGuid = Guid.create();
-    let srcGuid = Guid.create();
-    let testGuid = Guid.create();
-    let starterKitGuid = Guid.create();
-    let integrationGuid = Guid.create();
-    let unitGuid = Guid.create();
+    let solutionItemsGuid = uuidv4();
+    let srcGuid = uuidv4();
+    let testGuid = uuidv4();
+    let starterKitGuid = uuidv4();
+    let integrationGuid = uuidv4();
+    let unitGuid = uuidv4();
 
-    let applicationGuid = this.props.hasAppConfig ? this.props.appConfigGuid.trim() : Guid.create().value;
+    let applicationGuid = this.props.hasAppConfig ? this.props.appConfigGuid.trim() : uuidv4();
 
     let applicationUmeName = this.props.hasUMEName ? this.props.appUMName.toUpperCase() : probableUmeName;
-
-    //console.log("APP UME NAME = " + applicationUmeName)
 
     let copyOptions = {
       process: function (contents) {
         let str = contents.toString();
-        let result = str.replace(/FOOBAR/g, projectName)
+        return str
+          .replace(/FOOBAR/g, projectName)
           .replace(/foobar/g, lowerProjectName)
-          .replace(/153C20F8-826E-4AE7-3087-BE77A1D2A988/g, solutionItemsGuid.value.toUpperCase())
-          .replace(/B41FC458-86EC-FF29-1691-C526F2000F0E/g, srcGuid.value.toUpperCase())
-          .replace(/4E6B5AAF-3ED4-7688-C19F-B32717889E89/g, testGuid.value.toUpperCase())
-          .replace(/6B9C8153-26AA-83A5-4B93-119E48875A86/g, starterKitGuid.value.toUpperCase())
-          .replace(/43A0EA6E-20EC-C3DE-C04C-D0E086D03DDA/g, integrationGuid.value.toUpperCase())
-          .replace(/48B7DFB0-4E5F-FDFC-CFFD-33B79F8C342F/g, unitGuid.value.toUpperCase())
+          .replace(/153C20F8-826E-4AE7-3087-BE77A1D2A988/g, solutionItemsGuid.toUpperCase())
+          .replace(/B41FC458-86EC-FF29-1691-C526F2000F0E/g, srcGuid.toUpperCase())
+          .replace(/4E6B5AAF-3ED4-7688-C19F-B32717889E89/g, testGuid.toUpperCase())
+          .replace(/6B9C8153-26AA-83A5-4B93-119E48875A86/g, starterKitGuid.toUpperCase())
+          .replace(/43A0EA6E-20EC-C3DE-C04C-D0E086D03DDA/g, integrationGuid.toUpperCase())
+          .replace(/48B7DFB0-4E5F-FDFC-CFFD-33B79F8C342F/g, unitGuid.toUpperCase())
           .replace(/AA8563B1-CC02-4F01-9FAC-E93764F81C9B/g, applicationGuid)
           .replace(/UMEFOO/g, applicationUmeName)
-          .replace(/TITLEFOO/g, applicationUmeName)
-        return result;
+          .replace(/TITLEFOO/g, applicationUmeName);
       }
     };
 
@@ -123,21 +128,37 @@ module.exports = yeoman.generators.Base.extend({
 
     // copy files and rename starterkit to projectName
 
-    console.log('Creation project skeleton...');
+    this.log('Creating project skeleton...');
 
-    nd.files(source, function (err, files) {
-      for (let i = 0; i < files.length; i++) {
-        let filename = files[i].replace(/FOOBAR/g, projectName)
+    nd.promiseFiles(source).then((files) => {
+      files.map((file, index) => {
+        let filename = file
+          .replace(/FOOBAR/g, projectName)
           .replace(/foobar/g, lowerProjectName)
           .replace(".npmignore", ".gitignore")
           .replace(source, dest);
-        //console.log(files[i] + ' --> ' + filename);
-        fs.copy(files[i], filename, copyOptions);
-      }
-    });
-  },
 
-  install: function () {
-    // this.installDependencies();
+        fs.copy(file, filename, copyOptions);
+
+        if (index === (files.length - 1)) {
+          done();
+        }
+      });
+    });
   }
-});
+
+  install() {
+    const packageJsonDir = `${process.cwd()}/src/${this.props.projectName}`;
+    process.chdir(packageJsonDir);
+
+    this.installDependencies({
+      bower: false,
+      npm: true
+    });
+  }
+
+  end() {
+    // Say goodbye
+    this.log(yosay('All done, Enjoy!'));
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,32 @@
 'use strict';
+const {src, series} = require('gulp');
 const path = require('path');
-const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const excludeGitignore = require('gulp-exclude-gitignore');
 const mocha = require('gulp-mocha');
 const istanbul = require('gulp-istanbul');
-const nsp = require('gulp-nsp');
 const plumber = require('gulp-plumber');
 
-gulp.task('static', function () {
-  return gulp.src('**/*.js')
+function runStatic() {
+  return src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+}
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
-
-gulp.task('pre-test', function () {
-  return gulp.src('generators\**\*.js')
+function runPreTest() {
+  return src('generators\**\*.js')
     .pipe(istanbul({
       includeUntested: true
     }))
     .pipe(istanbul.hookRequire());
-});
+}
 
-gulp.task('test', ['pre-test'], function (cb) {
+function runTest() {
   let mochaErr;
 
-  gulp.src('test/**/*.js')
+  src('test/**/*.js')
     .pipe(plumber())
     .pipe(mocha({reporter: 'spec'}))
     .on('error', function (err) {
@@ -41,7 +36,8 @@ gulp.task('test', ['pre-test'], function (cb) {
     .on('end', function () {
       cb(mochaErr);
     });
-});
+}
 
-gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+const test = series(runStatic, runPreTest, runTest);
+
+exports.test = test;
