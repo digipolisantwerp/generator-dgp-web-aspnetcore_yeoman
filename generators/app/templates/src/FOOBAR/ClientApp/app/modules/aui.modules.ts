3afafd8aafee061b116c6fc08bab4c6a901b1ad6
Florian Ramakers <florian.ramakers@continuous.be>
2020-10-21 09:29:46 +0200

    added popular acpaas components

--- generators/app/templates/src/FOOBAR/ClientApp/app/modules/aui.modules.ts
+++ generators/app/templates/src/FOOBAR/ClientApp/app/modules/aui.modules.ts
@@ -1,4 +1,17 @@
-// import your acpaas ui modules here
+/**
+ * Import acpaas modules here
+ * see: https://github.com/digipolisantwerp/acpaas-ui_angular for available components.
+ */
+import {TableModule} from '@acpaas-ui/ngx-table';
+import {ItemCounterModule, PaginationModule} from '@acpaas-ui/ngx-pagination';
+import {DatepickerModule} from '@acpaas-ui/ngx-forms';
+import {FooterModule, HeaderModule} from '@acpaas-ui/ngx-layout';
 
-
-export const AuiModules = [];
+export const AuiModules = [
+  HeaderModule,
+  FooterModule,
+  TableModule,
+  ItemCounterModule,
+  PaginationModule,
+  DatepickerModule
+];
