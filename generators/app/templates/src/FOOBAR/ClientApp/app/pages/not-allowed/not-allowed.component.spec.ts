f8050a669fa286e92a4c1857172d0c3c1628ad75
Jonah Jordan <Jonah.Jordan@digipolis.be>
2022-05-12 14:44:22 +0200

    FEAT: upgrade angular 11

--- generators/app/templates/src/FOOBAR/ClientApp/app/pages/not-allowed/not-allowed.component.spec.ts
+++ generators/app/templates/src/FOOBAR/ClientApp/app/pages/not-allowed/not-allowed.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NotAllowedComponent } from './not-allowed.component';
 
@@ -6,7 +6,7 @@ describe('NotAllowedComponent', () => {
   let component: NotAllowedComponent;
   let fixture: ComponentFixture<NotAllowedComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NotAllowedComponent ]
     })
